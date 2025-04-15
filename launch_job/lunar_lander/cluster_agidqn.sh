4773b6c83907fb8e9eebee8ee77cd54ad838f82e
Theo <theovincentjourdat@gmail.com>
2025-04-15 18:01:24 +0200

    extended running time

--- launch_job/lunar_lander/cluster_agidqn.sh
+++ launch_job/lunar_lander/cluster_agidqn.sh
@@ -5,6 +5,6 @@ parse_arguments $@
 
 echo "launch train $ALGO_NAME"
 
-sbatch --job-name $EXPERIMENT_NAME-$ALGO_NAME --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=3G --time=01:00:00 --partition amd,amd2,amd3 \
+sbatch --job-name $EXPERIMENT_NAME-$ALGO_NAME --array=$FIRST_SEED-$LAST_SEED --cpus-per-task=1 --mem-per-cpu=3G --time=02:00:00 --partition amd,amd2,amd3 \
     --output=experiments/$ENV_NAME/logs/$EXPERIMENT_NAME/$ALGO_NAME/train_$((N_PARALLEL_SEEDS * (FIRST_SEED - 1) + 1))-$((N_PARALLEL_SEEDS * LAST_SEED)).out \
     launch_job/$ENV_NAME/train.sh --algo_name $ALGO_NAME --env_name $ENV_NAME --experiment_name $EXPERIMENT_NAME $ARGS --n_parallel_seeds $N_PARALLEL_SEEDS
