36c5cb14d2ea34e2ea30d9bb53dcf36f17e2e062
Theo <theovincentjourdat@gmail.com>
2025-04-23 05:46:42 +0200

    better launcher

--- launch_job/atari/normal/local_dqn.sh
+++ launch_job/atari/normal/local_dqn.sh
@@ -10,6 +10,7 @@ fi
 
 tmux send-keys -t $TMUX_NAME "cd $(pwd)" ENTER
 tmux send-keys -t $TMUX_NAME "source env/bin/activate" ENTER
+tmux send-keys -t $TMUX_NAME "export WANDB_CACHE_DIR=wandb" ENTER
 FRACTION_GPU=$(echo "scale=2 ; 1 / ($LAST_SEED - $FIRST_SEED + 1)" | bc)
 tmux send-keys -t $TMUX_NAME "export XLA_PYTHON_CLIENT_MEM_FRACTION=$FRACTION_GPU" ENTER
 
