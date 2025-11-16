bb4b804528ae164fcef8536e1ea040f57ae18104
yogesh1q2w <yogesh120699@gmail.com>
2025-11-16 17:01:11 +0100

    launch file for mellowmax

--- launch_job/atari/launch.sh
+++ launch_job/atari/launch.sh
@@ -1,10 +1,10 @@
 SHARED_ARGS="--features 32 64 64 512 --replay_buffer_capacity 1_000_000 --batch_size 32 --update_horizon 1 --gamma 0.99 \
-    --horizon 27_000 --n_epochs 20 --n_training_steps_per_epoch 250_000 --data_to_update 4 --n_initial_samples 20_000 \
+    --horizon 27_000 --n_epochs 40 --n_training_steps_per_epoch 250_000 --data_to_update 4 --n_initial_samples 20_000 \
     --epsilon_end 0.01 --epsilon_duration 250_000 --learning_rate 6.25e-5"
 
-GAME="CrazyClimber"
+GAME="Breakout"
 N_BELLMAN_ITERATIONS=1  # 1 3 5 10
-LAYER_NORM=1  # 0 1
+LAYER_NORM=0  # 0 1
 BATCH_NORM=0  # 0 1
 ARCHITECTURE_TYPE="cnn"  # cnn impala
 TARGET_UPDATE_FREQ=8000
@@ -28,12 +28,12 @@ SHARED_ARGS="$SHARED_ARGS --target_update_frequency $TARGET_UPDATE_FREQ --archit
 SHARED_NAME="LN${LAYER_NORM}_BN${BATCH_NORM}_${ARCHITECTURE_TYPE}_T${TARGET_UPDATE_FREQ}"
 # ----- L2 Loss -----
 
-MMDQN_ARGS="--experiment_name L2_${SHARED_NAME}_${GAME}"
+MMDQN_ARGS="--experiment_name L2_MM_${SHARED_NAME}_${GAME}"
 launch_job/atari/${PLATFORM}_mmdqn.sh --first_seed 1 --last_seed 1 --n_parallel_seeds 2 $SHARED_ARGS $DQN_ARGS
 # launch_job/atari/${PLATFORM}_dqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS $DQN_ARGS
 launch_job/atari/${PLATFORM}_mmtfdqn.sh --first_seed 1 --last_seed 1 --n_parallel_seeds 2 $SHARED_ARGS $DQN_ARGS
 # launch_job/atari/${PLATFORM}_tfdqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS $DQN_ARGS
 
-MMISDQN_ARGS="--experiment_name L2_K${N_BELLMAN_ITERATIONS}_${SHARED_NAME}_${GAME} --n_bellman_iterations $N_BELLMAN_ITERATIONS"
+MMISDQN_ARGS="--experiment_name L2_MM_K${N_BELLMAN_ITERATIONS}_${SHARED_NAME}_${GAME} --n_bellman_iterations $N_BELLMAN_ITERATIONS"
 launch_job/atari/${PLATFORM}_mmisdqn.sh --first_seed 1 --last_seed 1 --n_parallel_seeds 2 $SHARED_ARGS $ISDQN_ARGS
 # launch_job/atari/${PLATFORM}_isdqn.sh --first_seed 5 --last_seed 5 --n_parallel_seeds 1 $SHARED_ARGS $ISDQN_ARGS
